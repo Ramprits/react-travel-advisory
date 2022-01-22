5178d1d16c75e7da3fa571d998117f6f4eb83b9e
Ramprit Sahani <rampritsahani@outlook.in>
2022-01-22 08:45:32 +0530

    initial commit with superplate

---
+++ src/components/css/logo/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { ReactIcon } from "components/icons";
+
+export const Logo: React.FC = () => {
+  return <ReactIcon data-test="icon" color="white" width="96" height="58" />;
+};
